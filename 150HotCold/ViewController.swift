c4156485e0203f266bf3aedffc45ae8acbd38f53
Roman <romazaniosdev@gmail.com>
2026-05-03 14:51:24 +0300

    Initial Commit

---
+++ 150HotCold/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  150HotCold
+//
+//  Created by Roman on 5/3/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
